70b3e20e891b137b8218c39add1e5216cee9b2b6
syjeffkim <107138664+syjeffkim@users.noreply.github.com>
2024-03-07 12:50:24 -0800

    add generate button and js to alert ther madlib sentence

---
+++ script.js
@@ -0,0 +1,10 @@
+const peopleDropdown = document.getElementById("people-dropdown");
+const verbsDropdown = document.getElementById("verbs-dropdown");
+const locationDropdown = document.getElementById("location-dropdown");
+const foodDropdown = document.getElementById("food-dropdown");
+const madLibTextResult = document.getElementById("madlib-text");
+const generateMadLibBtn = document.getElementById("generate-madlib-btn");
+
+generateMadLibBtn.addEventListener("click", () => {
+    alert("1. ___ rode the 2. ___ and used 3. ___ on the enemies!");
+  });
